82abdab25e595d0633e8a1b1a20e1304a04e9433
Emm4nuell <eduardo.eesf@gmail.com>
2025-03-22 13:11:53 -0300

    Feito ajustes na pagina Sobre Mim

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,12 +7,14 @@ type PropsType = {
 };
 const Card = ({ icons, titulo, children }: PropsType) => {
   return (
-    <div className="bg-[#262626]/60 border-white border-1 rounded-[14px] w-full">
-      <div className="border-2 border-[#FF6900] mt-5"></div>
+    <div className="group bg-[#262626]/60 border-white transition-all duration-300 hover:border-orange-500 hover:scale-102 border-1 rounded-[14px] w-full">
+      <div className="h-1 bg-gradient-to-r from-orange-500 to-[#FF6900] mt-10"></div>
       <div className="grid gap-5 p-4">
         {icons}
-        <h3 className="text-white font-black">{titulo}</h3>
-        {children}
+        <h3 className="text-white text-[22px] font-black group-hover:text-orange-500 group-transition-all group-duration-300">
+          {titulo}
+        </h3>
+        <div className="text-[14px] text-[#f0f8ff]">{children}</div>
       </div>
     </div>
   );
--- src/pages/SobreMin/SobreMin.tsx
+++ src/pages/SobreMin/SobreMin.tsx
@@ -1,111 +1,126 @@
 import Card from "@/components/Card";
-import { FaAddressCard, FaUserGraduate } from "react-icons/fa";
+import {
+  FaUserAlt,
+  FaGraduationCap,
+  FaBriefcase,
+  FaCertificate,
+  FaTools,
+} from "react-icons/fa";
 import fotoperfil from "@/assets/fotoperfil.jpeg";
 
 export default function SobreMin() {
   return (
-    <div className="flex flex-col items-center gap-5 p-4 md:p-15 bg-black/95 text-white w-full min-h-[90vh]">
-      <div className="flex flex-col items-center">
-        <div className="w-26 h-26 bg-orange-500 border-2 border-orange-500 rounded-full flex items-center justify-center text-2xl font-bold overflow-hidden">
+    <div className="flex flex-col items-center gap-6 p-6 md:p-16 bg-gray-900 text-white w-full min-h-[90vh]">
+      {/* Perfil */}
+      <div className="flex flex-col items-center text-center">
+        <div className="w-28 h-28 rounded-full overflow-hidden border-4 border-orange-500">
           <img
             src={fotoperfil}
-            alt="Foto"
-            className="w-26 h-26 rounded-[50px] scale-[1.2] border-2 border-orange-500"
+            alt="Foto de Eduardo Emmanuel"
+            className="w-full h-full object-cover"
           />
         </div>
-        <h2 className="text-xl font-semibold mt-4">Eduardo Emmanuel</h2>
-        <p className="text-orange-500">Desenvolvedor Full Stack</p>
+        <h2 className="text-2xl font-semibold mt-4">Eduardo Emmanuel</h2>
+        <p className="text-orange-400 text-lg">Desenvolvedor Full Stack</p>
       </div>
 
-      <Card
-        icons={
-          <FaAddressCard className="bg-[#404040] w-9 h-9 p-[6px] text-[#FE9A00] rounded-[6px]" />
-        }
-        titulo="Sobre Mim"
-      >
-        <strong>
-          Desenvolvedor Full Stack especializado em Spring Boot e boas práticas
-          de programação. Experiência na criação de APIs RESTful e sistemas
-          escaláveis, com foco em desempenho e experiência do usuário.
-        </strong>
-      </Card>
+      {/* Cards */}
+      <section className="flex flex-col gap-6 w-full max-w-3xl">
+        <Card
+          icons={
+            <FaUserAlt className="bg-[#404040] text-[#FE9A00] w-9 h-9 p-2 rounded-[10px]" />
+          }
+          titulo="Sobre Mim"
+        >
+          <p className="leading-relaxed">
+            Desenvolvedor Full Stack com experiência em{" "}
+            <strong>Spring Boot</strong> e <strong>React</strong>, focado em
+            arquitetura escalável, boas práticas e otimização de código.
+            Especializado na criação de APIs RESTful seguras e performáticas,
+            aplicando princípios <strong>SOLID</strong>,{" "}
+            <strong>Arquitetura Hexagonal</strong> e{" "}
+            <strong>Testes Automatizados</strong>. Busco aprimorar
+            constantemente minhas habilidades em engenharia de software para
+            desenvolver soluções robustas e eficientes.
+          </p>
+        </Card>
 
-      <Card
-        icons={
-          <FaUserGraduate className="bg-[#404040] w-9 h-9 p-[6px] text-[#FE9A00] rounded-[6px]" />
-        }
-        titulo="Formação Acadêmica"
-      >
-        <ul className="list-disc list-inside text-sm mt-2">
-          <strong>Pós-graduação (Em andamento): </strong>
-          <span>MBA em Full Stack Web Development - UNIESP (2024 - 2025)</span>
-          <li>
-            <strong>BACK-END: </strong>Spring Boot, Arquitetura Hexagonal,
-            RabbitMQ, Kafka, SOLID
-          </li>
-          <li>
-            <strong>FRONT-END: </strong>React, Next.js e Angular
-          </li>
-          <strong>Tecnólogo: </strong>
-          <span>Sistemas para Internet - FATECPB (2011 - 2014)</span>
-        </ul>
-      </Card>
+        <Card
+          icons={
+            <FaGraduationCap className="bg-[#404040] text-[#FE9A00] w-9 h-9 p-2 rounded-[10px]" />
+          }
+          titulo="Formação Acadêmica"
+        >
+          <ul className="list-none space-y-2 text-sm">
+            <li>
+              <strong>MBA (Em andamento): </strong> Full Stack Web Development -
+              UNIESP (2024 - 2025)
+            </li>
+            <li>
+              <strong>Tecnólogo: </strong> Sistemas para Internet - FATECPB
+              (2011 - 2014)
+            </li>
+            <li>
+              <strong>Especializações: </strong> Spring Boot, Arquitetura
+              Hexagonal, RabbitMQ, Kafka, SOLID, Next.js, Angular
+            </li>
+          </ul>
+        </Card>
 
-      <Card
-        icons={
-          <FaAddressCard className="bg-[#404040] w-9 h-9 p-[6px] text-[#FE9A00] rounded-[6px]" />
-        }
-        titulo="Experiência Profissional"
-      >
-        <p className="text-sm mt-2">
-          <strong>
-            SampaioForce (Trabalho Voluntário) - Full Stack Developer (2024 -
-            Presente)
-          </strong>
-        </p>
-        <ul className="list-disc list-inside text-sm mt-2">
-          <li>
-            Desenvolvimento de APIs RESTful com Spring Boot, JPA, Mysql e SOLID.
-          </li>
-          <li>Criação de interfaces com Next.js.</li>
-          <li>
-            Uso de Flyway para versionamento de banco de dados e GIT/GITHUB.
-          </li>
-        </ul>
-      </Card>
+        <Card
+          icons={
+            <FaBriefcase className="bg-[#404040] text-[#FE9A00] w-9 h-9 p-2 rounded-[10px]" />
+          }
+          titulo="Experiência Profissional"
+        >
+          <p className="text-sm">
+            <strong>
+              SampaioForce (Trabalho Voluntário) - Full Stack Developer (2024 -
+              Presente)
+            </strong>
+          </p>
+          <ul className="list-disc list-inside text-sm mt-2 space-y-1">
+            <li>
+              Desenvolvimento de APIs RESTful com Spring Boot, JPA e MySQL.
+            </li>
+            <li>Criação de interfaces modernas com Next.js.</li>
+            <li>Uso de Flyway para versionamento do banco de dados.</li>
+          </ul>
+        </Card>
 
-      <Card
-        icons={
-          <FaAddressCard className="bg-[#404040] w-9 h-9 p-[6px] text-[#FE9A00] rounded-[6px]" />
-        }
-        titulo="Certificações"
-      >
-        <ul className="list-disc list-inside text-sm mt-2">
-          <li>Spring Boot Expert - Segurança com JWT e boas práticas.</li>
-          <li>Microserviços e Mensageria - Spring Cloud e Docker.</li>
-          <li>Criação de Serviços Web com Spring Boot.</li>
-        </ul>
-      </Card>
+        <Card
+          icons={
+            <FaCertificate className="bg-[#404040] text-[#FE9A00] w-9 h-9 p-2 rounded-[10px]" />
+          }
+          titulo="Certificações"
+        >
+          <ul className="list-disc list-inside text-sm space-y-1">
+            <li>Spring Boot Expert - Segurança com JWT e boas práticas.</li>
+            <li>Microserviços e Mensageria - Spring Cloud e Docker.</li>
+            <li>Criação de Serviços Web com Spring Boot.</li>
+          </ul>
+        </Card>
 
-      <Card
-        icons={
-          <FaAddressCard className="bg-[#404040] w-9 h-9 p-[6px] text-[#FE9A00] rounded-[6px]" />
-        }
-        titulo="Habilidades"
-      >
-        <ul className="list-disc list-inside text-sm mt-2">
-          <li>Spring Boot & JPA</li>
-          <li>Next.js & React</li>
-          <li>Arquitetura Hexagonal</li>
-          <li>Docker & Microserviços</li>
-          <li>JWT & Segurança</li>
-          <li>TypeScript</li>
-          <li>Tailwind CSS</li>
-          <li>Flyway & Migrations</li>
-          <li>Testes Automatizados (JUnit)</li>
-          <li>Otimização de Queries SQL</li>
-        </ul>
-      </Card>
+        <Card
+          icons={
+            <FaTools className="bg-[#404040] text-[#FE9A00] w-9 h-9 p-2 rounded-[10px]" />
+          }
+          titulo="Habilidades"
+        >
+          <ul className="grid grid-cols-2 gap-2 text-sm">
+            <li>Spring Boot & JPA</li>
+            <li>Next.js & React</li>
+            <li>Arquitetura Hexagonal</li>
+            <li>Docker & Microserviços</li>
+            <li>JWT & Segurança</li>
+            <li>TypeScript</li>
+            <li>Tailwind CSS</li>
+            <li>Flyway & Migrations</li>
+            <li>Testes Automatizados (JUnit)</li>
+            <li>Otimização de Queries SQL</li>
+          </ul>
+        </Card>
+      </section>
     </div>
   );
 }
